713cde61cd9ba2aee39d6a633397cc8ff915d7e7
701-Coder <51012111+701-coder@users.noreply.github.com>
2022-02-24 11:05:29 +0800

    Add files via upload

---
+++ bash/bashJudge.sh
@@ -0,0 +1,39 @@
+prob_id="$1"
+c_file="$2"
+
+echo "------ JudgeGuest ------"
+echo "Problem ID: ${prob_id}"
+echo "Test on: ${c_file}"
+echo "------------------------"
+
+temp=`mktemp -d`
+
+#gcc -std=c99 -o ${temp}/a ${c_file} &> /dev/null
+
+#if [[ $? != 0 ]]; then
+#    echo "Compile Error"
+#    rm -r ${temp}
+#    exit
+#fi
+
+for ((i=0; ; i++)); do
+    wget --output-document ${temp}/${i}.in --no-check-certificate https://raw.githubusercontent.com/701-coder/testdata/main/bash/${prob_id}/${i}.in &> /dev/null
+    wget --output-document ${temp}/${i}.out --no-check-certificate https://raw.githubusercontent.com/701-coder/testdata/main/bash/${prob_id}/${i}.out &> /dev/null
+    if [[ $? != 0 ]]; then
+        break
+    fi
+    in=`cat ${temp}/${i}.in`
+    timeout --kill-after=60s 60s ./${c_file} ${in} > ${temp}/out
+    tle=$?
+    df=`diff ${temp}/${i}.out ${temp}/out`
+    if [[ ${tle} != 0 ]]; then
+        echo -e "${i}\tTime Limit Exceeded"
+    elif [[ ${df} == "" ]]; then
+        echo -e "${i}\tAccepted"
+    else
+        echo -e "${i}\tWrong Answer"
+    fi
+
+done
+
+rm -r ${temp}
